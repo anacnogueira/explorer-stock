ed284c0985c02b71ff487df7c0175a964663325f
Ana Claudia <anacnogueira@gmail.com>
2025-05-29 11:44:22 -0300

    Redirecting routes according to role

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -3,12 +3,28 @@ import { BrowserRouter } from "react-router-dom";
 import { useAuth } from "../hooks/auth";
 
 import { AdminRoutes } from "./admin.routes";
+import { CustomerRoutes } from "./customer.routes";
+import { SaleRoutes } from "./sale.routes";
 import { AuthRoutes } from "./auth.routes";
+import { USER_ROLES } from "../utils/roles";
 
 export function Routes() {
   const { user } = useAuth();
 
+  function AccessRoute() {
+    switch (user.role) {
+      case USER_ROLES.ADMIN:
+        return <AdminRoutes />;
+      case USER_ROLES.CUSTOMER:
+        return <CustomerRoutes />;
+      case USER_ROLES.SALE:
+        return <SaleRoutes />;
+      default:
+        return <CustomerRoutes />;
+    }
+  }
+
   return (
-    <BrowserRouter>{user ? <AdminRoutes /> : <AuthRoutes />}</BrowserRouter>
+    <BrowserRouter>{user ? <AccessRoute /> : <AuthRoutes />}</BrowserRouter>
   );
 }
